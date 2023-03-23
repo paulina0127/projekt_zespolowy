6e40fd03bd0d4e79424ec59d16894213ff0e3ec4
Alicja Dabrowska <alicja.dab27@gmail.com>
2023-03-23 22:29:35 +0100

    Add project skeleton

---
+++ backend/hiremenow_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HiremenowAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hiremenow_app'
---
+++ backend/hiremenow_app/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+# Create your views here.
