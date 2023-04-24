a7cc48dc6da696079b0e021dcb7a31f249dc29c5
DominikaJ <92153602+D0minikaJ@users.noreply.github.com>
2023-04-24 23:31:20 +0200

    Update Footer && HomeScreen Search Bar
    What's new:
    The search bar on home page is smaller.
    Footer got a new styling and content.
    
    To fix:
    Icons are to top left of input box, not inside of the box.
    "Dodaj ofertę pracy" in the footer isn't a link yet.
    Job card hover doesn't work.
    Size for smaller screens still isn't optimized.

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, {Fragment } from 'react';
 import styles from './Footer.module.css';
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className={styles["footer"]}>
-    <p>This is some content in sticky footer</p>
-    </div>
+    <>
+      <div className={styles["footer"]}>
+        <div className={styles["container"]}>
+          <div className="column align-items-center">
+            <h3 className={styles["footer-h3"]}>Znajdź swoją wymarzoną</h3>
+            <h3 className={styles["footer-h3"]}>ofertę i aplikuj z nami!</h3>
+          </div>
+          <div className="column">
+            <h1 className={styles["footer-h1"]}>Dla kandydatów</h1>
+            <Link to="/rejestracja">
+              <p className={styles["footer-p"]}>Zarejestruj się</p>
+            </Link>
+            <Link to="/oferty">
+              <p className={styles["footer-p"]}>Oferty pracy</p>
+            </Link>
+            <Link to="/pracodawcy">
+              <p className={styles["footer-p"]}>Pracodawcy</p>
+            </Link>
+          </div>
+          <div className="column">
+            <h1 className={styles["footer-h1"]}>Dla pracodawców</h1>
+            <Link to="/rejestracja">
+              <p className={styles["footer-p"]}>Zarejestruj się</p>
+            </Link>
+            <p className={styles["footer-p"]}>Dodaj ofertę pracy</p>
+          </div>
+          <div className="column">
+            <h1 className={styles["footer-h1"]}>Kontakt</h1>
+            <p className={styles["footer-p"]}>+48 000 000 000</p>
+            <p className={styles["footer-p"]}>hiremenow@gmail.com</p>
+            <p className={styles["footer-p"]}>Olsztyn, ul. Armii Krajowej 23</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
