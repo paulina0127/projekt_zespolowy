24009f85ff901c482ebfb804b5580933bbe0a590
Paulina Hryciuk <73948761+paulina0127@users.noreply.github.com>
2023-05-09 18:00:52 +0200

    Fix: file names

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -177,6 +177,7 @@ STATIC_URL = "/static/"
 # Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = "/media/"
+DEFAULT_FILE_STORAGE = "apps.core.utils.storage.CustomStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
